c0b0eed7fcdfdafe79af336b12cb2f20cfe9ff5b
Toni Uebernickel <tuebernickel@gmail.com>
2015-01-24 10:35:24 +0100

    clean up javascript
    * remove reading time
    * remove unnecessary libraries: fitvids, highlight, ghostHunter
    * remove GoSquared integration
    * consolidate javascript into index.js

--- js/index.js
+++ js/index.js
@@ -1,26 +1,57 @@
-/**
- * Main JS file for Casper behaviours
- */
-
-/*globals jQuery, document */
-(function ($) {
+(function (window, $, undefined) {
     "use strict";
 
-    $(document).ready(function(){
-        $(".post-content").fitVids();
+    $(function() {
+        // Apply animation of teaser images when scrolling down.
+        var $image = $('.post-image-image, .teaserimage-image');
+        if ($image.length) {
+            var $window = $(window);
 
-        // Calculates Reading Time
-        $('.post-content').readingTime({
-            readingTimeTarget: '.post-reading-time',
-            wordCountTarget: '.post-word-count',
+            $window.on('scroll', function() {
+                var top = $window.scrollTop();
+                if (top < 0 || top > 1500) {
+                    return;
+                }
+
+                $image
+                    .css('transform', 'translate3d(0px, '+top/3+'px, 0px)')
+                    .css('opacity', 1-Math.max(top/700, 0))
+                ;
+            });
+
+            $window.trigger('scroll');
+        }
+
+        // Apply padding to post, if there is a teaser article image present.
+        if ($('.article-image').length) {
+            var height = $('.article-image').height();
+            $('.post-content').css('padding-top', height + 'px');
+        }
+
+        // Apply smooth scrolling within the page itself.
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+                if (target.length) {
+                    $('html,body').animate({ scrollTop: target.offset().top }, 500);
+
+                    return false;
+                }
+            }
         });
 
-        // Creates Captions from Alt tags
+        // Create figures from images
         $(".post-content img").each(function() {
-            // Let's put a caption if there is one
-            if($(this).attr("alt"))
-              $(this).wrap('<figure class="image"></figure>')
-              .after('<figcaption>'+$(this).attr("alt")+'</figcaption>');
+            // Favor title over alt, if given.
+            var caption = $(this).attr("title") || $(this).attr("alt");
+            if (caption) {
+                $(this)
+                    .wrap('<figure class="image"></figure>')
+                    .after('<figcaption>'+caption+'</figcaption>')
+                ;
+            }
         });
     });
-}(jQuery));
+}(window, window.jQuery));
